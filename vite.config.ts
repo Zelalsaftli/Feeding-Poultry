44b0a87d59ead5e5cfde861497001131e890b819
Zelalsaftli <zelalsaftli@gmail.com>
2026-01-16 18:59:04 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => {
       host: '0.0.0.0',
     },
     plugins: [react()],
-    base: '/Feeding Poultry/', // اسم المستودع على GitHub
+    base: '/Feeding-Poultry/', // اسم المستودع على GitHub
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
